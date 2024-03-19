f151734fa6c65dfb0704567012d66c88d488f15d
suryaveeramani0044 <suryaveeramani0044@gmail.com>
2024-03-19 16:52:02 +0530

    loading texted added

--- src/page/loginPage.jsx
+++ src/page/loginPage.jsx
@@ -8,27 +8,36 @@ export const LoginPage = () => {
     email: "",
     password: "",
   });
+  const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
   const handleLogin = () => {
-    const { email, password } = user;
-    if (email && password) {
-      AxiosInstance.post("/login", user)
-        .then((res) => {
-          console.log("login data", res.data.token);
-          localStorage.setItem("jwttoken", res.data.token);
-          navigate("/dashboard");
-        })
-        .catch((e) => {
-          console.log(e);
-        });
-    } else {
-      alert("please fill all field");
+    try {
+      setLoading(true);
+      const { email, password } = user;
+      if (email && password) {
+        AxiosInstance.post("/login", user)
+          .then((res) => {
+            console.log("login data", res.data.token);
+            localStorage.setItem("jwttoken", res.data.token);
+            setLoading(false);
+            navigate("/dashboard");
+          })
+          .catch((e) => {
+            console.log(e);
+          });
+      } else {
+        alert("please fill all field");
+        setLoading(false);
+      }
+    } catch (e) {
+      console.log(e);
+      setLoading(false);
     }
   };
   return (
     <main className="login">
       <section className="login-container">
-        <h3 className="title">Chat Me</h3>
+        <h3 className="title">Welcome</h3>
         <input
           type="text"
           placeholder="Email or Username"
@@ -45,7 +54,7 @@ export const LoginPage = () => {
           onChange={(e) => setUser({ ...user, password: e.target.value })}
         />
         <button className="login-btn" onClick={() => handleLogin()}>
-          Login
+          {loading ? "loading..." : "Login"}
         </button>
         <p className="reg-text">Have a good day</p>
       </section>
